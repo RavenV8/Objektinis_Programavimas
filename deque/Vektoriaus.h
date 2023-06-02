eea226946a55d97d972077e079b12ae338eefd0d
RavenV8 <55328476+RavenV8@users.noreply.github.com>
2023-06-02 10:13:25 +0300

    Add files via upload

---
+++ deque/Vektoriaus.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <cstdlib>
+#include <numeric>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <stdio.h>
+
+#include <deque>
+using std::deque;
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::string;
+using std::vector;
+using std::numeric_limits;
+using std::stringstream;
+using std::to_string;
+using std::stoi;
+
+struct studentas{
+    string vardas, pavarde;
+    vector <int> paz;
+    int egz;
+    double bendr;
+};
